d1dcf8348e6826e55b91f50b20ccad976aecf64d
sreyan10 <149260595+sreyan10@users.noreply.github.com>
2024-02-21 14:10:41 -0600

    Checkpoint1.0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+#include <FEHUtility.h>
+#include <FEHServo.h>
 #include <FEHLCD.h>
+#include <FEHMotor.h>
 
+
+class Movement{ //checkpoint 1
+FEHMotor leftTire(FEHMotor::Motor0, 9);
+FEHMotor rightTire(FEHMotor::Motor1, 9);
+    Movement()
+    {
+        leftTire.setPercent(50);
+        rightTire.setPercent(50);
+    }
+    //given a distance we will calculate whats needed to make the robot move that much
+    void moveDistance(float distance)
+    {
+        
+    }
+    void toBoardingAndBack()
+    {
+
+    }
+};
 int main(void)
 {
     LCD.Clear(BLACK);
