ed81ee8c085fc48c1567d726212db26d5898f00d
JJ Kiratikosolrak <kkankanits@gmail.com>
2024-02-23 13:13:29 -0500

    Edit the class

--- main.cpp
+++ main.cpp
@@ -2,34 +2,39 @@
 #include <FEHServo.h>
 #include <FEHLCD.h>
 #include <FEHMotor.h>
-
-
-class Movement{ //checkpoint 1
 FEHMotor leftTire(FEHMotor::Motor0, 9);
 FEHMotor rightTire(FEHMotor::Motor1, 9);
-    Movement()
-    {
-        
-    }
-    //given a distance we will calculate whats needed to make the robot move that much
-    void moveDistance(float distance)
-    {
-        //PID tutorial
-    }
-    void toBoardingAndBack() //for checkpoint 1
-    {
 
-    }
-    void setLeftPercent(float value)
+class Tire{ //checkpoint 1
+
+    int speed;
+    Tire()
     {
-        leftTire.setPercent(value);
-    }
-    void setRightPercent(float value)
+        speed = 0;
+    } 
+    void setHalfSpeed()
     {
-        rightTire.setPercent(value);
+        speed = 25;
+    }
+    void setFullSpeed()
+    {
+        speed = 50;
+    }
+    int getSpeed() {
+        return speed;
     }
 
 };
+
+//given a distance we will calculate whats needed to make the robot move that much
+void moveDistance(float distance)
+{
+    //PID tutorial
+}
+void toBoardingAndBack() //for checkpoint 1
+{
+
+}
 int main(void)
 {
     LCD.Clear(BLACK);
