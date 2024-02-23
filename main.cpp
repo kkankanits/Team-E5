d934999bf2f2a80804e22de46a1bc19a82be9ef4
JJ Kiratikosolrak <kkankanits@gmail.com>
2024-02-23 14:00:58 -0500

    time code for checkpoint 1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <FEHMotor.h>
 FEHMotor leftTire(FEHMotor::Motor0, 9);
 FEHMotor rightTire(FEHMotor::Motor1, 9);
-
+AnalogInputPin cds(FEHIO::P0_0);
 
 class Movement{
 Movement(int rightSpeed, int leftSpeed)
@@ -30,34 +30,75 @@ void setLeftSpeed(int speed)
 }
 };
 
+void waitForStartLight() {
+    //detect any light (will have to change to red)
+    while(cds.value() >= 3.0) {
+
+    }
+}
+
+void driveForward(float sec) {
+    leftTire.SetPercent(50);
+    rightTire.SetPercent(50);
+    Sleep(sec);
+}
+
+void turnLeft(float sec) {
+    //left tire speed should be 0 to turn left
+    leftTire.SetPercent(0);
+    rightTire.SetPercent(50);
+    Sleep(sec);
+}
+
+void turnRight(float sec) {
+    //right tire speed should be 0 to turn right
+    leftTire.SetPercent(50);
+    rightTire.SetPercent(0);
+    Sleep(sec);
+}
+
+void turnLeftBackward(float sec) {
+    //left tire speed should be 0 to turn left
+    leftTire.SetPercent(0);
+    rightTire.SetPercent(-50);
+    Sleep(sec);
+}
+
+void stop() {
+    leftTire.SetPercent(0);
+    rightTire.SetPercent(0);
+    Sleep(1.0);
+}
+
 void checkpoint1() {
     //wait for the start light
-
+    waitForStartLight();
     //move the robot _ seconds
-
+    driveForward(3.0);
     //turn left halfway?
-
+    turnleft(2.0);
     //drive forward _ seconds
-
+    driveForward(2.0);
     //turn right 90 degree
-
+    turnRight(4.0);
     //drive forward _ seconds (up the ramp)
-
+    driveForward(6.0);
     //turn right slightly
-
+    turnRight(2.0);
     //drive forward _second
-
+    driveForward(2.0);
     //turn left slightly
-
+    turnLeft(2.0);
     //drive forward _ seconds (until hit the ticket kiosk)
-
+    driveForward(4.0);
     //turn left backward
-    
+    turnLeftBackward(4.0);
     //turn right forward
-
+    turnRight(4.0);
     //drive forward (down the ramp)
-
+    driveForward(5.0);
     //stop
+    stop();
 
     
 }
