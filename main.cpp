a5d208ca9cfe504048a71611da211d0bbd177478
sreyan10 <149260595+sreyan10@users.noreply.github.com>
2024-02-23 13:34:41 -0600

    Movement

--- main.cpp
+++ main.cpp
@@ -5,34 +5,12 @@
 FEHMotor leftTire(FEHMotor::Motor0, 9);
 FEHMotor rightTire(FEHMotor::Motor1, 9);
 
-class Tire{ //checkpoint 1
 
-    int speed;
-    Tire()
-    {
-        speed = 0;
-    } 
-    void resetSpeed(){
-        speed = 0;
-    }
-
-    void setHalfSpeed()
-    {
-        speed = 25;
-    }
-    void setFullSpeed()
-    {
-        speed = 50;
-    }
-    int getSpeed() {
-        return speed;
-    }
-    void setSpeed(int s) {
-        speed = s+1;
-    }
-
-};
-
+class Movement{
+Movement(int rightSpeed, int leftSpeed)
+{
+   //default 
+}
 //given a distance we will calculate whats needed to make the robot move that much
 void moveDistance(float distance)
 {
@@ -42,10 +20,22 @@ void toBoardingAndBack() //for checkpoint 1
 {
 
 }
+void setRightSpeed(int speed)
+{
+    rightTire.setPercent(speed);
+}
+void setLeftSpeed(int speed)
+{
+    leftTire.setPercent(speed);
+}
+};
+
+
 int main(void)
 {
     LCD.Clear(BLACK);
     LCD.WriteLine("Hello, World!");
     LCD.WriteLine("Hello, World!");
+    
     return 0;
 }
