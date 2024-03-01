dd75a8f6a91cf8e13222205409e8d222a40b75fe
Ayush Chakravarty <ayushchak05@gmail.com>
2024-02-29 19:14:04 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -212,7 +212,6 @@ void checkpoint1A() {
     //wait for the start light
     waitForStartLight();
     LCD.Write("HI");
-    LCD.Update();
     //move the robot out the starting position
     moveForwardDistance(20);
 
