c5d765ff92533781a9f086bd5f11dd46a96e1f63
JJ Kiratikosolrak <kkankanits@gmail.com>
2024-03-06 01:36:38 -0500

    Added checkpoint1 back for reference

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 
 #define WHEEL_Radius 1.25
 #define TRANSLATIONS_PER_REV 318.0
-#define PI 3.14
 
 
 AnalogInputPin cds(FEHIO::P3_7);
@@ -19,6 +18,50 @@ DigitalEncoder right_encoder(FEHIO::P0_0);
 DigitalEncoder left_encoder(FEHIO::P0_1);
 FEHMotor right_motor(FEHMotor::Motor3,9.0);
 FEHMotor left_motor(FEHMotor::Motor1,9.0);
+
+/*
+// This function programmed the robot for checkpoint 1 using distance 
+void checkpoint1A() {
+    //wait for the start light
+    waitForStartLight();
+    
+    //move the robot out the starting position
+    moveForwardDistance(20);
+    //turn left (halfway) to drive toward the ramp
+    turnLeft(.37);
+    //move forward to reach the ramp
+    moveForwardDistance(23);
+    
+    //turn right (90 degrees) to face the ramp
+    turnRight(.72);
+    //drive forward up the ramp 
+    moveForwardDistance(46);
+    //turn right (90 degree) to face the passport stamp
+    turnRight(.6);
+    //move forward towards the passport stamp
+    moveForwardDistance(25);
+    //turn left (90 degree) to face the ticket kiosk
+    turnLeft(.5);
+    //drive towards the ticket kiosk to touch
+    moveForwardDistance(33);
+    //drive backward towards the luggage
+    moveBackwardDistance(25);
+    //turn right backward to face the wall of the robot course
+    turnRightBackward(.65);
+    //drive to be in front of top of ramp
+    moveForwardDistance(18.5);
+    
+    //drive backward towards the passport stamp to make space for turning
+    //moveBackwardDistance(1.5);
+    //turn left (90 degree) to face the ramp
+    turnLeft(.705);
+    //turnLeftBackward(0.7);
+    //drive down the ramp
+    moveForwardDistance(45);
+    //stop
+    stop();
+}
+*/
 
 /* This function waits for the red start light to trigger the robot to start */
 void waitForStartLight() {
@@ -169,27 +212,6 @@ void checkpoint2()
         //turn towards button
         turn_left(50, 300);
     }
-
-
-    
-    // // if red, press red button
-    // if (checkColorLight) {
-    //     move_forward(-25, distance_to_count(9));
-
-    //     turn_right(50, 100);
-
-    //     move_forward(25, distance_to_count(6));
-
-    // } else { // press blue button
-    //     move_forward(-25, distance_to_count(9));
-
-    //     turn_right(50, 100);
-
-    //     move_forward(25, distance_to_count(10));
-    // }
-
-    
-
 
 }
 
