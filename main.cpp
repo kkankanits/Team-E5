13175776a2d3ab0c0b8388f979972b2d9c90e90f
JJ Kiratikosolrak <kkankanits@gmail.com>
2024-03-08 11:19:21 -0500

    Code up until the last run open lab 3/7 (blue light just need to go down the ramp, red light need to be fixed to press the button)

--- main.cpp
+++ main.cpp
@@ -66,16 +66,14 @@ void checkpoint1A() {
 /* This function waits for the red start light to trigger the robot to start */
 void waitForStartLight() {
     //detect any light (will have to change to red)
-    while(cds.Value() >= 2.0) {
-        
-    }
+    while(cds.Value() >= 2.0);
 }
 
 int distance_to_count(int distance){
     return (distance*TRANSLATIONS_PER_REV)/(2*M_PI*WHEEL_Radius);
 }
 
-void move_forward(int percent, int counts) //using encoders 
+void move_forward(int percent, int counts, float timeout) //using encoders 
 {
     //Reset encoder counts
     right_encoder.ResetCounts();
@@ -84,37 +82,37 @@ void move_forward(int percent, int counts) //using encoders
     //Set both motors to desired percent
     right_motor.SetPercent(percent);
     left_motor.SetPercent(percent);
+
+    float start = TimeNow();
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
-    while(((left_encoder.Counts() + right_encoder.Counts()) / 2.) < counts);
+    while((((left_encoder.Counts() + right_encoder.Counts()) / 2.) < counts) && (TimeNow() - start < timeout));
 
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
 
-void move_backward(int percent, int counts)
+void move_backward(int percent, int counts, float timeout)
 {
-    move_forward(-1*percent, counts);
+    move_forward(-1*percent, counts, timeout);
 }
 
-void turn_right(int percent, int counts) //using encoders 50, 250 for 90 degree
+void turn_right(int percent, int counts, float timeout) //using encoders 50, 250 for 90 degree
 {
-
     //Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
 
-    //Set both motors to desired percent
-    //hint: set right motor backwards, left motor forwards
+    //set right motor backwards, left motor forwards
     right_motor.SetPercent(-1*percent);
     left_motor.SetPercent(percent);
 
+    float start = TimeNow();
 
     //While the average of the left and right encoder is less than counts, keep running motors
-
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+    while(((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts) && (TimeNow() - start < timeout));
 
     //Turn off motors
     right_motor.Stop();
@@ -122,20 +120,20 @@ void turn_right(int percent, int counts) //using encoders 50, 250 for 90 degree
 
 }
 
-void turn_left(int percent, int counts) //using encoders 50, 250 for 90 degrees
+void turn_left(int percent, int counts, float timeout) //using encoders 50, 250 for 90 degrees
 {
-
     //Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
 
-    //Set both motors to desired percent
-    //hint: set right motor backwards, left motor forwards
+    //set right motor backwards, left motor forwards
     right_motor.SetPercent(percent);
     left_motor.SetPercent(-1*percent);
 
+    float start = TimeNow();
+
     //While the average of the left and right encoder is less than counts, keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+    while(((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts) && (TimeNow() - start < timeout));
 
     //Turn off motors
     right_motor.Stop();
@@ -147,7 +145,7 @@ void turn_left(int percent, int counts) //using encoders 50, 250 for 90 degrees
 bool isRedLight() {
     //blue is less than 2, red is less than 1
     //LCD.Write(cds.Value());
-    if(cds.Value() <= 1.1) {
+    if(cds.Value() <= 1.15) {
         return 1;
     }
     else {
@@ -161,81 +159,90 @@ void checkpoint2()
     Sleep(0.5);
 
     //hit the start button
-    move_backward(25, distance_to_count(1.8));
+    move_backward(25, distance_to_count(1.8), 2.);
 
     // get out of start light area
-    move_forward(25, distance_to_count(16));
+    move_forward(25, distance_to_count(16), 15.);
     
     // turn left facing the fuel switches
-    turn_left(50, 120);
+    turn_left(50, 120, 5.);
 
     // forward towards the left steeper ramp
-    move_forward(25, distance_to_count(10.8));
+    move_forward(25, distance_to_count(10.8), 15.);
 
     // turn right to face ramp
-    turn_right(50, 310);
+    turn_right(50, 325, 10.0);
 
     // get up ramp and closer to color light
-    move_forward(25, distance_to_count(37));
+    move_forward(25, distance_to_count(27), 20.);
+
+    //turn right after got up the ramp bc it steer to the left
+    turn_right(50, 40, 2.0);
+
+    // get up ramp and closer to color light
+    move_forward(25, distance_to_count(9.7), 10.);
 
     // turn towards light (slight right turn)
-    turn_right(50, 155);
+    turn_right(50, 170, 5.);
 
     // move to get the sensor above the color light 
-    move_forward(25, distance_to_count(8));
+    move_forward(25, distance_to_count(8), 4.);
 
     Sleep(1.0);
+
+    float distanceToButton;
+    int turntoButton;
+    float distanceToRamp;
+    bool red = 0;
 
     if(isRedLight()) {
         LCD.Clear(BLACK);
         LCD.Write("RED");
-
-        //move backward a little to give space for turning
-        move_backward(25, distance_to_count(2));
-
-        //turn to make the arm face the ticket kiosk
-        turn_right(50, 140);
-        
-        //drive towards button
-        move_forward(25, distance_to_count(12));
-
-        //turn towards button
-        turn_left(50, 280);
-
-        //drive towards button
-        move_forward(25, distance_to_count(2));
-
-        //move backward away from ticket kiosk
-        move_backward(25, distance_to_count(10));
-
-        //turn towards ramp
-        turn_left(50, 280);
+        distanceToButton = 11;
+        turntoButton = 250;  
+        distanceToRamp = 11;
+        red = 1;
     }
     else {
         LCD.Clear(BLACK);
         LCD.Write("BLUE");
-
-        //move backward a little to give space for turning
-        move_backward(25, distance_to_count(2));
-
-        //turn to make the arm face the ticket kiosk
-        turn_right(50, 140);
-
-        //drive towards button
-        move_forward(25, distance_to_count(8.3));
-
-        //turn towards button
-        turn_left(50, 265);
-
-        //drive towards button
-        move_forward(25, distance_to_count(2));
-
-        //move backward away from ticket kiosk
-        move_backward(25, distance_to_count(10));
-
-        //turn towards ramp
-        turn_left(50, 280);
+        distanceToButton = 12;
+        turntoButton = 270;  
+        distanceToRamp = 8.5;
     }
+
+    //move backward a little to give space for turning
+    move_backward(25, distance_to_count(8), 5.);
+
+    //turn to make the arm face the ticket kiosk
+    turn_right(50, 150, 5.);
+
+    //drive towards button
+    move_forward(25, distance_to_count(distanceToButton), 10.);
+
+    //turn towards button
+    turn_left(50, turntoButton, 5.);
+
+    //drive backward from button (for space)
+    move_backward(25, distance_to_count(2), 5.);
+
+    //drive towards button
+    move_forward(25, distance_to_count(7), 5.);
+
+    //move backward away from ticket kiosk
+    move_backward(25, distance_to_count(10), 15.);
+
+    //turn towards ramp
+    turn_left(50, 280, 5.);
+
+    //move forward to ramp
+    move_forward(25, distance_to_count(distanceToRamp), 15.);
+
+    //turn left to go down the ramp
+    turn_left(50, 275, 5.);
+
+    //move forward down the ramp
+    move_forward(25, distance_to_count(15), 30.);
 
 }
 
